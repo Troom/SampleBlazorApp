d15f44966629f6dc4f9e7f6e3005c806e3ed9e70
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-03-29 14:22:52 +0200

    Added primitive pagination Will be updated with mudBlazor.

--- ClientApp/Pages/OrdersOverview.razor.cs
+++ ClientApp/Pages/OrdersOverview.razor.cs
@@ -9,18 +9,44 @@ namespace ClientApp.Pages
         [Inject]
         public IOrderDataService? OrderDataService { get; set; }
 
+        public List<OrderDto> AllOrders { get; set; } = default!;
         public List<OrderDto> Orders { get; set; } = default!;
         private OrderDto? _selectedOrder;
 
         private string Title = "Orders overview";
 
+        public int PageNumber { get; set; } = 1;
+        const int Offset = 10;
+        public int NumberOfOrders;
+
+
         protected override async Task OnInitializedAsync()
         {
-            Orders = (await OrderDataService.GetAllOrders()).ToList();
+            AllOrders = (await OrderDataService.GetAllOrders()).ToList();
+            Orders = AllOrders.Skip((PageNumber-1)*Offset).Take(Offset).ToList();
+            NumberOfOrders = AllOrders.Count;
         }
 
         public void ShowViewPopup(OrderDto selectedOrder) { 
             _selectedOrder = selectedOrder;
+        }
+
+        public void PreviousPage()
+        {
+            if ((PageNumber-1) >= 1)
+            {
+                PageNumber--;
+                Orders = AllOrders.Skip((PageNumber - 1) * Offset).Take(Offset).ToList();
+            }
+        }
+
+        public void NextPage()
+        {
+            if (PageNumber <= AllOrders.Count/Offset)
+            {
+                PageNumber++;
+                Orders = AllOrders.Skip((PageNumber - 1) * Offset).Take(Offset).ToList();
+            }
         }
     }
 }
